67fce289bd529deab9ee5b5c2ca18a4b333a24d3
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-09-09 23:33:56 +0600

    re build

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,14 @@ const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ["http://localhost:5173", "https://car-rental-reservation-system-frontend-mbc2bxvua.vercel.app"], credentials: true }));
+app.use((0, cors_1.default)({
+    origin: [
+        "http://localhost:5173",
+        "https://car-rental-reservation-system-frontend-tau.vercel.app",
+        "https://car-rental-reservation-system-frontend-mbc2bxvua.vercel.app",
+    ],
+    credentials: true,
+}));
 // application routes
 app.use("/api", routes_1.default);
 app.get("/", (req, res) => {
